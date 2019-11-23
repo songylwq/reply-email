23587affd2283c3c858a2fedbc3900b2a8833d0c
songylwq <songylwq@126.com>
2019-11-23 17:37:07 +0800

    搞清楚了怎么获取邮件状态了，下一步研究一下怎么筛选，怎么更新到服务器

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/emersion/go-imap/client"
 	"github.com/emersion/go-imap"
+	"time"
 )
 
 func main() {
@@ -26,66 +27,86 @@ func main() {
 	}
 	log.Println("Logged in")
 
-	// List mailboxes
-	mailboxes := make(chan *imap.MailboxInfo, 10)
-	done := make(chan error, 1)
-	go func () {
-		done <- c.List("", "*", mailboxes)
-	}()
+	//// List mailboxes
+	//mailboxes := make(chan *imap.MailboxInfo, 10)
+	//done := make(chan error, 1)
+	//go func () {
+	//	done <- c.List("", "*", mailboxes)
+	//	//done <- c.List("", "INBOX", mailboxes)
+	//}()
+	//
+	//log.Println("Mailboxes:")
+	//for m := range mailboxes {
+	//	log.Println("* " + m.Name)
+	//}
+	//
+	//log.Println("******")
+	//
+	//if err := <-done; err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//log.Println("Select INBOX")
+	//
+	//// Select INBOX
+	//mbox, err := c.Select("INBOX", false)
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//log.Println("Flags for INBOX:", mbox.Flags)
+	//
+	////mbox.Flags = []string{"\\Deleted"}
+	////mbox.PermanentFlags = []string{"\\Deleted"}
+	////
+	////log.Println("Flags 修改后:", mbox.Flags)
+	////log.Println("PermanentFlags 修改后:", mbox.PermanentFlags)
+	//
+	//// Get the last 4 messages
+	//from := uint32(1)
+	//to := mbox.Messages
+	//if mbox.Messages > 2 {
+	//	// We're using unsigned integers here, only substract if the result is > 0
+	//	from = mbox.Messages - 2
+	//}
+	//seqset := new(imap.SeqSet)
+	//seqset.AddRange(from, to)
+	//
+	//log.Println("mbox.Messages : ", mbox.Messages)
+	//log.Println("mbox.PermanentFlags : ", mbox.PermanentFlags)
 
-	log.Println("Mailboxes:")
-	for m := range mailboxes {
-		log.Println("* " + m.Name)
-	}
 
-	log.Println("******")
-
-	if err := <-done; err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Select INBOX")
-
-	// Select INBOX
-	mbox, err := c.Select("[]INBOX", false)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	c.Search()
-
-	log.Println("Flags for INBOX:", mbox.Flags)
-
-	// Get the last 4 messages
-	from := uint32(1)
-	to := mbox.Messages
-	if mbox.Messages > 5 {
-		// We're using unsigned integers here, only substract if the result is > 0
-		from = mbox.Messages - 5
-	}
 	seqset := new(imap.SeqSet)
-	seqset.AddRange(from, to)
+	seqset.AddRange(183, 185)
 
-	log.Println("mbox.Messages:", mbox.Messages)
 
 	messages := make(chan *imap.Message, 10)
-	done = make(chan error, 1)
-	go func() {
-		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
-	}()
+	done := make(chan error, 1)
 
-	log.Println("Last 4 messages:")
-	for msg := range messages {
-		log.Println("* " + msg.Envelope.Subject)
-		log.Println("from: ", msg.Envelope.From[0])
-		log.Println("to: ", msg.Envelope.To[0])
-		log.Println("ReplyTo: ", msg.Envelope.ReplyTo[0])
+	for i:=1;i<10;i++{
+		go func() {
+			done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope, imap.FetchFlags}, messages)
+		}()
+		time.Sleep(3 * time.Second)
+
+		log.Println("Last 4 messages:")
+		for msg := range messages {
+			log.Println("* " + msg.Envelope.Subject)
+			log.Println("*,Flags : ", msg.Flags)
+			//log.Println("from: ", msg.Envelope.From[0])
+			//log.Println("to: ", msg.Envelope.To[0])
+			//log.Println("ReplyTo: ", msg.Envelope.ReplyTo[0])
+			//log.Println("MessageId: ", msg.Envelope.MessageId)
+		}
+
+		if err := <-done; err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	if err := <-done; err != nil {
-		log.Fatal(err)
-	}
+	//回复邮件
+
 
 	log.Println("Done!")
 }
